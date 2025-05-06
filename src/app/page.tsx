08eb7afb32d786027721bde5092f2cb6c2fd494a
Ukyo <ukyou20011121@gmail.com>
2025-05-06 23:59:18 +0900

    add: shadcnでボタン追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Title from "./components/Title";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <Title />
+      <Button />
     </div>
   );
 }
