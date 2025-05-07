c6212c0e2f3416f4477fcb19a51d69b76a658a27
Ukyo <ukyou20011121@gmail.com>
2025-05-08 00:10:27 +0900

    add: ルームを作成ボタンと参加ボタンの作成

---
+++ src/app/createroom/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Createroom() {
+  return (
+    <div className="min-h-[calc(100vh-48px)] flex flex-col items-center justify-center"></div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,12 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-[calc(100vh-48px)] flex flex-col items-center justify-center">
       <Title />
-      <Button />
+      <div className="flex pt-5 gap-4">
+        <Button>ルームを作成</Button>
+        <Button>ルームに参加</Button>
+      </div>
     </div>
   );
 }
