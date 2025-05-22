7fb5e93a88d7a192e2100cbb21b021ea7b86e79a
Ukyo <ukyou20011121@gmail.com>
2025-05-22 15:51:28 +0900

    delete: 作成、参加ページ削除

--- src/app/joinroom/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-
-export default function Joinroom() {
-  return (
-    <div className="min-h-[calc(100vh-48px)] flex flex-col items-center justify-center"></div>
-  );
-}
